b91d42a728a74af8db2baf0f6ef0141e67cd610b
bwolfe2 <bwolfe2@atu.edu>
2024-08-27 10:37:11 -0500

    Commit 1

--- Practice2/Practice2.cpp
+++ Practice2/Practice2.cpp
@@ -6,5 +6,6 @@ int main()
 {
     cout << "Hello World!" << endl;
     cout << "1st commit to see if it's working" << endl;
+    cout << "2nd commit to double check if it's working!" << endl;
     // get fetch to see the changes on visual studios.
 }
