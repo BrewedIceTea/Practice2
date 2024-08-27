d0083f3e36af2481ce5719c5fae56ed305555c87
bwolfe2 <bwolfe2@COR235-TL08>
2024-08-27 10:27:05 -0500

    Add project files.

---
+++ Practice2/Practice2.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "1st commit to see if it's working" << endl;
+    // get fetch to see the changes on visual studios.
+}
